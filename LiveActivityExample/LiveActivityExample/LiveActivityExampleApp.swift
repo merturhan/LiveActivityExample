83e61a022a0dc27380bf7bbce5ee4a6920842ae9
Mert Urhan <mert-urhan@hotmail.com>
2024-09-08 14:38:41 +0300

    Initial Commit
    Project created

---
+++ LiveActivityExample/LiveActivityExample/LiveActivityExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveActivityExampleApp.swift
+//  LiveActivityExample
+//
+//  Created by Mert Urhan on 8.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct LiveActivityExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
